6ad18453954121066362e89118ac8e9af83b8997
Benjamin <benjamin@mirusresearch.com>
2019-11-20 10:39:20 -0600

    attempt to add homepage settings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import BlogList from '../components/BlogList';
 
+import useSiteMetadata from '../static_queries/useSiteMetadata';
+import Img from 'gatsby-image';
+const FeaturedPhoto = ({ title, description, photo }) => {
+  return <Img src={require(photo)} alt={`${title}: ${description}`} />;
+};
 export default function IndexPage() {
+  const { featured_photos } = useSiteMetadata();
   return (
     <Layout page="home">
       <section>
-        <BlogList />
+        {featured_photos.map(photo => (
+          <FeaturedPhoto {...photo} />
+        ))}
       </section>
     </Layout>
   );
